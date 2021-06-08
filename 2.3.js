bed2a1cf1521b66665f184395c41d31dcf69f05d
[Gyuzyal] <[gyuzyal.yassynova@gmail.com]>
2021-06-08 16:37:17 +0200

    Adding 2.3

---
+++ 2.3.js
@@ -0,0 +1,19 @@
+// Display even numbers
+
+    // version 1
+let i = 1;
+while (i <= 100){ 
+    if (i%2 == 0){
+      console.log(i);  
+    }
+    i++;
+}
+
+    // version 2
+
+let j = 1;
+for (;j <= 50;j++){ 
+    if (j%2 == 0){
+      console.log(j);  
+    }
+}
